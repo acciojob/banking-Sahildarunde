75ca344e3f9744e53b81646df971e0ea591c5564
Sahil Darunde <sahildarunde007@gmail.com>
2023-05-04 16:40:39 +0530

    bankAcc updated

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -9,22 +9,46 @@ import java.util.Random;
 
 import static java.lang.Math.min;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class BankAccount {
 
     private String name;
     private double balance;
     private double minBalance;
 
+    public BankAccount(String name, double balance, double minBalance) {
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
+
     public String generateAccountNumber(int digits, int sum) throws Exception{
-        //Each digit of an account number can lie between 0 and 9 (both inclusive)
-        //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
-        //If it is not possible, throw "Account Number can not be generated" exception
-        if(sum<0 || sum>digits*9){
-            throw new AccountNumberCannotBeGeneratedException("Account number cannot generated");
+        if(sum < 0 || sum > digits*9) {
+            throw new AccountNumberCannotBeGeneratedException("Account Number can not be generated");
         }
         String acNo = "";
         Random rand = new Random();
@@ -37,19 +61,23 @@ public class BankAccount {
             remainingSum -= n;
         }
         return acNo;
+        //Each digit of an account number can lie between 0 and 9 (both inclusive)
+        //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
+        //If it is not possible, throw "Account Number can not be generated" exception
     }
 
     public void deposit(double amount) {
-        //add amount to balance
         this.balance += amount;
+        //add amount to balance
+
     }
 
     public void withdraw(double amount) throws Exception {
-        // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-        if(this.balance - amount < this.minBalance){
-            throw new Exception("Insufficient balance");
+        if(this.balance - amount < this.minBalance) {
+            throw new Exception("Insufficient Balance");
         }
         this.balance -= amount;
+        // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
     }
 
 }
